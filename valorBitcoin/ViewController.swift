639db39192fb7d20eff694e65c0a6aa6a01a1b85
mac ssd <macssd@mbp-de-mac.home>
2017-09-08 21:24:03 -0300

    Initial Commit

---
+++ valorBitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  valorBitcoin
+//
+//  Created by mac ssd on 08/09/17.
+//  Copyright © 2017 andruino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
